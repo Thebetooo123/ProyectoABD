5e192ec5d63e2591fb1038e5f06ee67246d22993
beto <lopezporras617@gmail.com>
2018-12-01 13:08:12 -0700

    .

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     //Si existe una sesión correcta, mostramos la página para los usuarios
     //Sino, mostramos la página de acceso y registro de usuarios
     if(isset($_SESSION['usuario']) and $_SESSION['estado'] == 'Autenticado') {
-        include('pagina.php');
+        include('reportes/pagina.php');
         die();
     } else {
         include('login.php');
